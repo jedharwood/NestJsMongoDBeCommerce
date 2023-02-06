2b8390d273d57526cc5e09dcc5b78d9f267a2530
Jed Harwood <scaryteeth@hotmail.com>
2023-02-06 12:26:15 +0900

    implementing guards on auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Get,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dtos/create-user.dto';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { Roles } from './decorators/roles.decorator';
+import { Role } from './enums/role.enum';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { LocalAuthGuard } from './guards/local.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  @Post('/register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.addUser(createUserDto);
+    return user;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User)
+  @Get('/user')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Get('/admin')
+  getDashboard(@Request() req) {
+    return req.user;
+  }
+}
--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Role } from 'src/auth/enums/role.enum';
 
 export type UserDocument = User & Document;
 
@@ -13,6 +14,9 @@ export class User {
 
   @Prop()
   password: string;
+
+  @Prop()
+  roles: Role[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
